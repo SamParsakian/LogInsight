8cfde426e0919b3275ef87cfd4b7a78532213cb1
samuel.parsakian@yh.a <samkian0007@gmail.com>
2026-02-28 15:29:45 +0100

    refactor: add scene switching helpers

--- src/main/java/com/logmonitoring/application/MainApplication.java
+++ src/main/java/com/logmonitoring/application/MainApplication.java
@@ -15,10 +15,36 @@ public class MainApplication extends Application {
         showLoginView();
     }
 
-    private void showLoginView() throws Exception {
+    public void showLoginView() throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/login.fxml"));
         Scene scene = new Scene(loader.load());
         primaryStage.setTitle("Log Monitoring Tool - Login");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void showMainView() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/main.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("Log Monitoring Tool");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(true);
+        primaryStage.setMaximized(true);
+        primaryStage.show();
+    }
+
+    public void showStatisticsView() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/statistics.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("Log Monitoring Tool - Graph View");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void showReportsView() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/log_details.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("Log Monitoring Tool - Reports");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
